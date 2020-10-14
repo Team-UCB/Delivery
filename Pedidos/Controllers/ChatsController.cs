f448f24c4da02213177276fd1ff6491525a76da2
GAKUZM <gabrielzmav@gmail.com>
2020-10-14 18:30:37 -0400

    sprint2 corrigiendo metodos faltantes

---
+++ Pedidos/Controllers/ChatsController.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Pedidos.Models;
+
+namespace Pedidos.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ChatsController : ControllerBase
+    {
+        private readonly PedidosPollomonContext _context;
+
+        public ChatsController(PedidosPollomonContext context)
+        {
+            _context = context;
+        }
+
+        /// <summary>
+        /// Gets All the Chats from the BDB
+        /// </summary>
+        /// <returns></returns>
+        // GET: api/Chats/columna/direccion
+        [Helpers.Authorize]
+        [HttpGet]
+        public async Task<ActionResult<PageAndSortResponse<Chat>>> GetChat([FromQuery] PageAndSortRequest param)
+        {
+            IEnumerable<Chat> listaChat = null;
+            if (param.Direccion.ToLower() == "asc")
+                listaChat = await _context.Chats.OrderBy(p => EF.Property<object>(p, param.Columna)).ToListAsync();
+            else if (param.Direccion.ToLower() == "desc")
+                listaChat = await _context.Chats.OrderByDescending(p => EF.Property<object>(p, param.Columna)).ToListAsync();
+            else
+                listaChat = await _context.Chats.OrderBy(p => p.Id).ToListAsync();
+
+            if (listaChat == null)
+            {
+                return NotFound();
+            }
+
+            int total = 0;
+            if (!string.IsNullOrEmpty(param.Filtro))
+            {
+                listaChat = listaChat.Where(ele => ele.Estado.Contains(param.Filtro));
+            }
+            total = listaChat.Count();
+            listaChat = listaChat.Skip((param.Pagina - 1) * param.TamPagina).Take(param.TamPagina);
+
+            var result = new PageAndSortResponse<Chat>
+            {
+                Datos = listaChat,
+                TotalFilas = total
+            };
+
+            return result;
+        }
+
+
+
+
+
+
+
+
+        // GET: api/Chats
+        //[HttpGet]
+        //public async Task<ActionResult<IEnumerable<Chat>>> GetChat()
+        //{
+        //    return await _context.Chat.ToListAsync();
+        //}
+
+
+        /// <summary>
+        /// Get a Chat specific for Id from the BDB
+        /// </summary>
+        /// <returns></returns>
+        // GET: api/Chats/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Chat>> GetChat(long id)
+        {
+            var chat = await _context.Chats.FindAsync(id);
+
+            if (chat == null)
+            {
+                return NotFound();
+            }
+
+            return chat;
+        }
+
+        /// <summary>
+        /// Modifies an existing Chat
+        /// </summary>
+        /// <param name="chat"></param>
+        /// <returns></returns>
+        // PUT: api/Chats/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutChat(long id, Chat chat)
+        {
+            if (id != chat.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(chat).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ChatExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Chats
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPost]
+        public async Task<ActionResult<Chat>> PostChat(Chat chat)
+        {
+            _context.Chats.Add(chat);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetChat", new { id = chat.Id }, chat);
+        }
+
+        // DELETE: api/Chats/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Chat>> DeleteChat(long id)
+        {
+            var chat = await _context.Chats.FindAsync(id);
+            if (chat == null)
+            {
+                return NotFound();
+            }
+
+            _context.Chats.Remove(chat);
+            await _context.SaveChangesAsync();
+
+            return chat;
+        }
+
+        private bool ChatExists(long id)
+        {
+            return _context.Chats.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ Pedidos/Controllers/PedidosController.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Pedidos.Models;
+
+namespace Pedidos.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PedidosController : ControllerBase
+    {
+        private readonly PedidosPollomonContext _context;
+
+        public PedidosController(PedidosPollomonContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Pedido/columna/direccion
+        [Helpers.Authorize]
+        [HttpGet]
+        public async Task<ActionResult<PageAndSortResponse<Pedido>>> GetPedidos([FromQuery] PageAndSortRequest param)
+        {
+            IEnumerable<Pedido> listaPedidos = null;
+            if (param.Direccion.ToLower() == "asc")
+                listaPedidos = await _context.Pedidos.OrderBy(p => EF.Property<object>(p, param.Columna)).ToListAsync();
+            else if (param.Direccion.ToLower() == "desc")
+                listaPedidos = await _context.Pedidos.OrderByDescending(p => EF.Property<object>(p, param.Columna)).ToListAsync();
+            else
+                listaPedidos = await _context.Pedidos.OrderBy(p => p.Id).ToListAsync();
+
+            if (listaPedidos == null)
+            {
+                return NotFound();
+            }
+
+            int total = 0;
+            if (!string.IsNullOrEmpty(param.Filtro))
+            {
+                listaPedidos = listaPedidos.Where(ele => ele.CodigoQrFactura.Equals(param.Filtro));
+            }
+            total = listaPedidos.Count();
+            listaPedidos = listaPedidos.Skip((param.Pagina - 1) * param.TamPagina).Take(param.TamPagina);
+
+            var result = new PageAndSortResponse<Pedido>
+            {
+                Datos = listaPedidos,
+                TotalFilas = total
+            };
+
+            return result;
+        }
+
+        // GET: api/Pedidos/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Pedido>> GetPedido(long id)
+        {
+            var pedido = await _context.Pedidos.FindAsync(id);
+
+            if (pedido == null)
+            {
+                return NotFound();
+            }
+
+            return pedido;
+        }
+
+        // PUT: api/Pedidos/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutPedido(long id, Pedido pedido)
+        {
+            if (id != pedido.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(pedido).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!PedidoExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Pedidos
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPost]
+        public async Task<ActionResult<Pedido>> PostPedido(Pedido pedido)
+        {
+            _context.Pedidos.Add(pedido);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetPedido", new { id = pedido.Id }, pedido);
+        }
+
+        // DELETE: api/Pedidos/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Pedido>> DeletePedido(long id)
+        {
+            var pedido = await _context.Pedidos.FindAsync(id);
+            if (pedido == null)
+            {
+                return NotFound();
+            }
+
+            _context.Pedidos.Remove(pedido);
+            await _context.SaveChangesAsync();
+
+            return pedido;
+        }
+
+        private bool PedidoExists(long id)
+        {
+            return _context.Pedidos.Any(e => e.Id == id);
+        }
+    }
+}
