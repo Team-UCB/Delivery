e60decb34b690d6b5e89c4856d4137221804b3b1
GAKUZM <gabrielzmav@gmail.com>
2020-11-16 01:18:55 -0400

    test vendedor mensaje detallepedido y metodo de vista productovendedor

---
+++ TestUnitarios/TestDetallePedido.cs
@@ -0,0 +1,300 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.VisualBasic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Pedidos.Controllers;
+using Pedidos.Models;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TestUnitarios
+{
+    [TestClass]
+    public class TestDetallePedido
+    {
+        private static long idDetallePedido = -1;
+        static PedidosPollomonContext pedidos = new PedidosPollomonContext();
+        DetallePedidosController detallepedidosController = new DetallePedidosController(pedidos);
+        [TestMethod]
+        public async Task PostDetallePedidoTest()
+        {
+
+            var result = await detallepedidosController.PostDetallePedido(new DetallePedido() { 
+                Cantidad =1,
+                SubMonto=1,
+                IdPedidoNavigation= new Pedido()
+                {
+                    FechaIngreso = DateTime.Now,
+                    FechaAtencion = DateTime.Now,
+                    FechaSalida = DateTime.Now,
+                    FechaEntrega = DateTime.Now,
+                    Estado = "activo",
+                    CodigoQrFactura = "codigo qr de prueba",
+                    MontoEnvio = 123,
+                    TipoPago = "efectivo",
+                    MontoCliente = 12,
+                    IdClienteNavigation = new Cliente()
+                    {
+                        NombresApellidos = "Pepe Galleta",
+                        Celular = "78547845",
+                        Telefono = "46657812"
+                    },
+                    IdTransporteNavigation = new Transportador()
+                    {
+                        Celular = "78547845",
+                        DescripcionVehiculo = "Moto",
+                        Estado = "activo",
+                        Latitud = 0M,
+                        Longitud = 0M,
+                        NombreCompleto = "Pedro Flores",
+                        TipoVehiculo = "Moto",
+                    },
+                    IdVendedorNavigation = new Vendedor()
+                    {
+                        NombreEmpresa = "Micro mercado la terminal",
+                        Celular = "78547845",
+                        Telefono = "46657812",
+                        Direccion = "Av. La Paz esq. Av. Victor Paz",
+                        Correo = "laterminalmm@gmail.com",
+                        PersonaContacto = "Juan Perez",
+                        PathLogo = "",
+                        IdRubroNavigation = new Rubro()
+                        {
+                            Nombre = "Super Mercados",
+                            Descripcion = "Nuevo Rubro"
+                        }
+                    }
+                },
+                IdProductoNavigation = new Producto
+                {
+                    Nombre = "Coca cola 2lts.",
+                    PrecioUnitario = 15,
+                    Cantidad = 2,
+                    PrecioMayor = 10,
+                    Marca = "Embol S.A",
+                    Modelo = "Coca-Cola",
+                    Especificaciones = "Gaseosa negra de 2 lts",
+                    IdCategoriaNavigation = new CategoriaProducto
+                    {
+                        Nombre = "Gaeosas",
+                        Descripcion = "liquidos en base a componentes gaseoss",
+                        Lugar = "Sección refrigerantes"
+                    },
+                    IdVendedorNavigation = new Vendedor
+                    {
+                        PersonaContacto = "Pedro Fuentes Lazarraga",
+                        Celular = "72967356",
+                        Telefono = "466-23058",
+                        Correo = "carlos.fuentes@hotmail.com",
+                        NombreEmpresa = "Arcor",
+                        Direccion = "Zona mercader Av. Ballivian",
+                        PathLogo = "",
+                        IdRubroNavigation = new Rubro
+                        {
+                            Nombre = "Comida Rápida",
+                            Descripcion = "encargados del área de comida rapida y derivados"
+                        }
+                    }
+                }
+            });
+
+
+            Assert.IsNotNull(result.Result);
+            DetallePedido detallepedido = (DetallePedido)(result.Result as CreatedAtActionResult).Value;
+            Assert.IsNotNull(detallepedido);
+            Assert.IsTrue(detallepedido.Id > 0);
+            idDetallePedido = detallepedido.Id;
+        }
+        [TestMethod]
+        public async Task UpdateDetallePedidoTest()
+        {
+
+            var result = await detallepedidosController.PutDetallePedido(idDetallePedido, new DetallePedido()
+            {
+                Cantidad = 2000,
+                SubMonto = 1,
+                IdPedidoNavigation = new Pedido()
+                {
+                    FechaIngreso = DateTime.Now,
+                    FechaAtencion = DateTime.Now,
+                    FechaSalida = DateTime.Now,
+                    FechaEntrega = DateTime.Now,
+                    Estado = "activo",
+                    CodigoQrFactura = "codigo qr de prueba",
+                    MontoEnvio = 123,
+                    TipoPago = "efectivo",
+                    MontoCliente = 12,
+                    IdClienteNavigation = new Cliente()
+                    {
+                        NombresApellidos = "Pepe Galleta",
+                        Celular = "78547845",
+                        Telefono = "46657812"
+                    },
+                    IdTransporteNavigation = new Transportador()
+                    {
+                        Celular = "78547845",
+                        DescripcionVehiculo = "Moto",
+                        Estado = "activo",
+                        Latitud = 0M,
+                        Longitud = 0M,
+                        NombreCompleto = "Pedro Flores",
+                        TipoVehiculo = "Moto",
+                    },
+                    IdVendedorNavigation = new Vendedor()
+                    {
+                        NombreEmpresa = "Micro mercado la terminal",
+                        Celular = "78547845",
+                        Telefono = "46657812",
+                        Direccion = "Av. La Paz esq. Av. Victor Paz",
+                        Correo = "laterminalmm@gmail.com",
+                        PersonaContacto = "Juan Perez",
+                        PathLogo = "",
+                        IdRubroNavigation = new Rubro()
+                        {
+                            Nombre = "Super Mercados",
+                            Descripcion = "Nuevo Rubro"
+                        }
+                    }
+                },
+                IdProductoNavigation = new Producto
+                {
+                    Nombre = "Coca cola 2lts.",
+                    PrecioUnitario = 15,
+                    Cantidad = 2,
+                    PrecioMayor = 10,
+                    Marca = "Embol S.A",
+                    Modelo = "Coca-Cola",
+                    Especificaciones = "Gaseosa negra de 2 lts",
+                    IdCategoriaNavigation = new CategoriaProducto
+                    {
+                        Nombre = "Gaeosas",
+                        Descripcion = "liquidos en base a componentes gaseoss",
+                        Lugar = "Sección refrigerantes"
+                    },
+                    IdVendedorNavigation = new Vendedor
+                    {
+                        PersonaContacto = "Pedro Fuentes Lazarraga",
+                        Celular = "72967356",
+                        Telefono = "466-23058",
+                        Correo = "carlos.fuentes@hotmail.com",
+                        NombreEmpresa = "Arcor",
+                        Direccion = "Zona mercader Av. Ballivian",
+                        PathLogo = "",
+                        IdRubroNavigation = new Rubro
+                        {
+                            Nombre = "Comida Rápida",
+                            Descripcion = "encargados del área de comida rapida y derivados"
+                        }
+                    }
+                }
+            });
+            Assert.IsNotNull(result);
+        }
+
+        [TestMethod]
+        public async Task DeleteDetallePedidoTest()
+        {
+
+            var result = await detallepedidosController.DeleteDetallePedido(idDetallePedido);
+
+            Assert.IsNotNull(result);
+        }
+        [TestMethod]
+        public async Task Add99DetallePedidoTest()
+        {
+
+            for (int i = 0; i < 99; i++)
+            {
+                var result = await detallepedidosController.PostDetallePedido(new DetallePedido()
+                {
+                    Cantidad = i,
+                    SubMonto = i,
+                    IdPedidoNavigation = new Pedido()
+                    {
+                        FechaIngreso = DateTime.Now,
+                        FechaAtencion = DateTime.Now,
+                        FechaSalida = DateTime.Now,
+                        FechaEntrega = DateTime.Now,
+                        Estado = "activo",
+                        CodigoQrFactura = "codigo qr de prueba",
+                        MontoEnvio = 123,
+                        TipoPago = "efectivo",
+                        MontoCliente = 12,
+                        IdClienteNavigation = new Cliente()
+                        {
+                            NombresApellidos = "Pepe Galleta",
+                            Celular = "78547845",
+                            Telefono = "46657812"
+                        },
+                        IdTransporteNavigation = new Transportador()
+                        {
+                            Celular = "78547845",
+                            DescripcionVehiculo = "Moto",
+                            Estado = "activo",
+                            Latitud = 0M,
+                            Longitud = 0M,
+                            NombreCompleto = "Pedro Flores",
+                            TipoVehiculo = "Moto",
+                        },
+                        IdVendedorNavigation = new Vendedor()
+                        {
+                            NombreEmpresa = "Micro mercado la terminal",
+                            Celular = "78547845",
+                            Telefono = "46657812",
+                            Direccion = "Av. La Paz esq. Av. Victor Paz",
+                            Correo = "laterminalmm@gmail.com",
+                            PersonaContacto = "Juan Perez",
+                            PathLogo = "",
+                            IdRubroNavigation = new Rubro()
+                            {
+                                Nombre = "Super Mercados",
+                                Descripcion = "Nuevo Rubro"
+                            }
+                        }
+                    },
+                    IdProductoNavigation = new Producto
+                    {
+                        Nombre = "Coca cola 2lts.",
+                        PrecioUnitario = 15,
+                        Cantidad = 2,
+                        PrecioMayor = 10,
+                        Marca = "Embol S.A",
+                        Modelo = "Coca-Cola",
+                        Especificaciones = "Gaseosa negra de 2 lts",
+                        IdCategoriaNavigation = new CategoriaProducto
+                        {
+                            Nombre = "Gaeosas",
+                            Descripcion = "liquidos en base a componentes gaseoss",
+                            Lugar = "Sección refrigerantes"
+                        },
+                        IdVendedorNavigation = new Vendedor
+                        {
+                            PersonaContacto = "Pedro Fuentes Lazarraga",
+                            Celular = "72967356",
+                            Telefono = "466-23058",
+                            Correo = "carlos.fuentes@hotmail.com",
+                            NombreEmpresa = "Arcor",
+                            Direccion = "Zona mercader Av. Ballivian",
+                            PathLogo = "",
+                            IdRubroNavigation = new Rubro
+                            {
+                                Nombre = "Comida Rápida",
+                                Descripcion = "encargados del área de comida rapida y derivados"
+                            }
+                        }
+                    }
+                });
+                Assert.IsNotNull(result);
+            }
+        }
+        [TestMethod]
+        public async Task GetDetallePedidosTest()
+        {
+            var result = await detallepedidosController.GetRubro(new PageAndSortRequest() { Pagina = 1, TamPagina = 10, Columna = "Id", Direccion = "asc", Filtro = "" });
+
+            Assert.IsNotNull(result);
+            Assert.IsTrue(result.Value.Datos.Count() > 0);
+
+        }
+    }
+}
---
+++ TestUnitarios/TestVendedor.cs
@@ -0,0 +1,115 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Pedidos.Controllers;
+using System;
+using Pedidos.Models;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+
+namespace TestUnitarios
+{
+    [TestClass]
+    public class TestVendedor
+    {
+        private static long idVendedor = -1;
+        static PedidosPollomonContext pedidosContext = new PedidosPollomonContext();
+        VendedoresController vendedoresController = new VendedoresController(pedidosContext);
+
+        [TestMethod]
+        public async Task Add99VendedorTest()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                var result = await vendedoresController.PostVendedor(new Vendedor()
+                {
+                    PersonaContacto = "Prueba2",
+                    Celular = "100",
+                    Telefono = "100",
+                    Correo = "prueba",
+                    NombreEmpresa = "prueba2",
+                    Direccion = "prueba",
+                    PathLogo = "",
+                    IdRubroNavigation = new Rubro
+                    {
+                        Nombre = "Prueba",
+                        Descripcion = "Prueba",
+                        IdPadre = 1
+                    }
+                });
+                Assert.IsNotNull(result);
+            }
+        }
+
+        [TestMethod]
+        public async Task Add()
+        {
+
+            var result = await vendedoresController.PostVendedor(new Vendedor()
+            {
+                PersonaContacto = "Prueba",
+                Celular = "100",
+                Telefono = "100",
+                Correo = "prueba",
+                NombreEmpresa = "prueba2",
+                Direccion = "prueba",
+                PathLogo = "",
+                IdRubroNavigation = new Rubro
+                {
+                    Nombre = "Prueba",
+                    Descripcion = "Prueba",
+                    IdPadre = 1
+                }
+            });
+            Assert.IsNotNull(result.Result);
+            Vendedor vendedor = (Vendedor)(result.Result as CreatedAtActionResult).Value;
+            Assert.IsNotNull(vendedor);
+            Assert.IsTrue(vendedor.Id > 0);
+            idVendedor = vendedor.Id;
+        }
+        [TestMethod]
+        public async Task PutVendedorTest()
+        {
+
+            var result = await vendedoresController.PutVendedor(idVendedor, new Vendedor()
+            {
+                PersonaContacto = "Prueba2",
+                Celular = "100",
+                Telefono = "100",
+                Correo = "prueba",
+                NombreEmpresa = "prueba2",
+                Direccion = "prueba",
+                PathLogo = "",
+                IdRubroNavigation = new Rubro
+                {
+                    Nombre = "Prueba",
+                    Descripcion = "Prueba",
+                    IdPadre = 1
+                }
+            });
+            Assert.IsNotNull(result);
+        }
+        [TestMethod]
+        public async Task DeleteVendedorTest()
+        {
+
+            var result = await vendedoresController.DeleteVendedor(idVendedor);
+            Assert.IsNotNull(result);
+        }
+        [TestMethod]
+        public async Task GetVendedorTest()
+        {
+
+            var result = await vendedoresController.GetVendedor(new PageAndSortRequest()
+            {
+                Columna = "Id",
+                Direccion = "asc",
+                Pagina = 1,
+                TamPagina = 10,
+                Filtro = ""
+            });
+
+            Assert.IsNotNull(result);
+            Assert.IsTrue(result.Value.Datos.Count() > 0);
+        }
+    }
+}
